123daca81b741fdbdb9427621e2799f8f995949d
Somesh Joshi <someshjoshi1007@gmail.com>
2022-12-15 16:18:04 +0530

    Remove Hardcoded Values & Implement Dynamic Logic

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 const container = document.querySelector('.container')
 const changeSizeButton = document.querySelector('.change-size')
 const resetButton = document.querySelector('.reset-button')
+const DEFAULT_GRID_SIZE = 16
 
 changeSizeButton.addEventListener('click', changeGridSize)
 resetButton.addEventListener('click', resetGrid)
-generateGrid(16)
+generateGrid(DEFAULT_GRID_SIZE)
 
 function generateGrid(gridSize) {
-    container.style.cssText = `grid-template-columns: repeat(${gridSize}, 0fr); grid-template-rows: repeat(${gridSize} , 0fr)`
-    let height = 700 / gridSize
-    let width = 700 / gridSize
-    for (let i = 1; i <= gridSize; i++) {
-        for (let j = 1; j <= gridSize; j++) {
-            let div = document.createElement('div')
-            div.classList.add('square')
-            div.style.height = height + 'px'
-            div.style.width = width + 'px'
-            div.addEventListener('mouseover', changeColor)
-            container.appendChild(div)
-        }
+    container.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize} , 1fr)`
+    let height = container.offsetHeight / gridSize
+    let width = container.offsetWidth / gridSize
+    for (let i = 1; i <= gridSize ** 2; i++) {
+        let div = document.createElement('div')
+        div.classList.add('square')
+        div.style.height = height + 'px'
+        div.style.width = width + 'px'
+        div.addEventListener('mouseover', changeColor)
+        container.appendChild(div)
     }
 }
 
 function changeGridSize() {
-    let size = +prompt('Enter Number (10-100) :', 16)
+    let size = +prompt('Enter Number (10-100) :', DEFAULT_GRID_SIZE)
 
     if (isNaN(size) || (size < 10 || size > 100)) {
         alert('Invalid Number')
